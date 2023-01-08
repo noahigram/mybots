5a0f310ef539f0494bf776d2afa118173a1f27da
noahigram <noahigram2023@u.northwestern.edu>
2023-01-08 16:34:11 -0600

    Task D commit before last part

--- generate.py
+++ generate.py
@@ -1,4 +1,27 @@
 import pyrosim.pyrosim as pyrosim
-pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0, 0, 0.5], size=[1, 1, 1])
+pyrosim.Start_SDF("boxes.sdf")
+l = 1
+w = 1
+h = 1
+x = 0
+y = 0
+z = 0.5
+# Create 10 blocks with a for loop
+# for ii in range(10):
+#     newl = length*(0.9**ii)
+#     neww = width*(0.9**ii)
+#     newh = height*(0.9**ii)
+
+#     pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[
+#                       newl,neww,newh])
+#     z += newh
+
+# Create 25 towers
+for ii in range(5):
+    for jj in range(5):
+        for kk in range(10):
+            pyrosim.Send_Cube(
+                name="Box", pos=[x+ii, y+jj, z], size=[l*(0.9**kk), w*(0.9**kk), h*(0.9**kk)])
+            z += 1
+
 pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -1,7 +1,12 @@
 import pybullet as p
+import pybullet_data
 import time as t
+
 physicsClient = p.connect(p.GUI)
-p.loadSDF("box.sdf")
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.setGravity(0, 0, -9.8)
+planeID = p.loadURDF("plane.urdf")
+p.loadSDF("boxes.sdf")
 for ii in range(1000):
     p.stepSimulation()
     print(ii)
