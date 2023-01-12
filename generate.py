9ceeda8d2c2bd42f1f144718ea24e1a5d08a0097
noahigram <noahigram2023@u.northwestern.edu>
2023-01-12 16:58:04 -0600

    Part F completed commit

--- generate.py
+++ generate.py
@@ -11,12 +11,13 @@ def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
 
     # Robot with 3 links and two joints (Torso, Backleg and frontleg)
-    pyrosim.Send_Cube(name="BackLeg", pos=[0, 0, 0.5], size=[1, 1, 1])
-    pyrosim.Send_Joint(name="BackLeg_Torso", parent="BackLeg",
-                       child="Torso", type="revolute", position=[0.5, 0, 1])
-    pyrosim.Send_Cube(name="Torso", pos=[0.5, 0, 0.5], size=[1, 1, 1])
+    pyrosim.Send_Cube(name="Torso", pos=[1.5, 0, 1.5], size=[1, 1, 1])
+
+    pyrosim.Send_Joint(name="Torso_BackLeg", parent="Torso",
+                       child="BackLeg", type="revolute", position=[1, 0, 1])
+    pyrosim.Send_Cube(name="BackLeg", pos=[-0.5, 0, -0.5], size=[1, 1, 1])
     pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso",
-                       child="FrontLeg", type="revolute", position=[1, 0, 0])
+                       child="FrontLeg", type="revolute", position=[2, 0, 1])
     pyrosim.Send_Cube(name="FrontLeg", pos=[0.5, 0, -0.5], size=[1, 1, 1])
     pyrosim.End()
 
--- simulate.py
+++ simulate.py
@@ -1,6 +1,8 @@
 import pybullet as p
 import pybullet_data
 import time as t
+import pyrosim.pyrosim as pyrosim
+import numpy as np
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -8,9 +10,20 @@ p.setGravity(0, 0, -9.8)
 planeID = p.loadURDF("plane.urdf")
 robotID = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
-for ii in range(1000):
+pyrosim.Prepare_To_Simulate(robotID)
+simLength = 300
+backLegSensorValues = np.zeros(simLength)
+frontLegSensorValues = np.zeros(simLength)
+
+for ii in range(simLength):
     p.stepSimulation()
-    print(ii)
+    backLegSensorValues[ii] = pyrosim.Get_Touch_Sensor_Value_For_Link(
+        "BackLeg")
+    frontLegSensorValues[ii] = pyrosim.Get_Touch_Sensor_Value_For_Link(
+        "FrontLeg")
+
     t.sleep(1/60)
 
+np.save("data/backLegSensorValues.npy", backLegSensorValues)
+np.save("data/frontLegSensorValues.npy", frontLegSensorValues)
 p.disconnect()
