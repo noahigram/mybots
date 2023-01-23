7847f7c7fcf9d95201e8f7c86d442ce057cc7725
noahigram <noahigram2023@u.northwestern.edu>
2023-01-23 17:25:40 -0600

    Finished part J

--- generate.py
+++ generate.py
@@ -29,7 +29,12 @@ def Generate_Brain():
     pyrosim.Send_Sensor_Neuron(name=2, linkName="FrontLeg")
     pyrosim.Send_Motor_Neuron(name=3, jointName="Torso_BackLeg")
     pyrosim.Send_Motor_Neuron(name=4, jointName="Torso_FrontLeg")
-
+    pyrosim.Send_Synapse(sourceNeuronName=0, targetNeuronName=3, weight=0.1)
+    pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=3, weight=0.1)
+    pyrosim.Send_Synapse(sourceNeuronName=2, targetNeuronName=4, weight=0.1)
+    pyrosim.Send_Synapse(sourceNeuronName=0, targetNeuronName=4, weight=0.1)
+    pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=4, weight=4.0)
+    pyrosim.Send_Synapse(sourceNeuronName=2, targetNeuronName=3, weight=0.1)
     pyrosim.End()
 
 
--- pyrosim/neuron.py
+++ pyrosim/neuron.py
@@ -71,10 +71,23 @@ class NEURON:
         self.Set_Value(
             pyrosim.Get_Touch_Sensor_Value_For_Link(self.Get_Link_Name()))
 
-    def Update_Hidden_Or_Motor_Neuron(self):
+    def Update_Hidden_Or_Motor_Neuron(self, neurons, synapses):
         self.Set_Value(0)
 
+        for key in synapses.keys():
+            if key[1] == self.Get_Name():
+
+                self.Allow_Presynaptic_Neuron_To_Influence_Me(
+                    synapses[key].Get_Weight(), neurons[key[0]].Get_Value())
+
+        self.Threshold()
+
+    def Allow_Presynaptic_Neuron_To_Influence_Me(self, weight, value):
+        self.Add_To_Value(weight*value)
+
+
 # -------------------------- Private methods -------------------------
+
 
     def Determine_Name(self, line):
 
